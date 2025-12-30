efe0a48ed8ca1ebd6e4cd0172a72df274703ba79
SahooDebasish <debasish.sahoo@gmail.com>
2025-12-30 09:31:31 +0530

    this is my new Comment program.

---
+++ src/ex_02_Variables_DataTypes/Lab008_Comments.java
@@ -0,0 +1,14 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab008_Comments {
+    // Comment practice
+    // Comment practice
+    // Comment practice
+    public static void main(String[] args) {
+        /** I am Deb
+        I can write anything in the comment
+         Java is easy
+        System.out.println();
+         **/
+    }
+}
