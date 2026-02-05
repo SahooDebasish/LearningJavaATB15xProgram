8b643b1e79e53db9667aaa0abf7af596d660c360
SahooDebasish <debasish.sahoo@gmail.com>
2026-02-05 07:37:31 +0530

    this is my Loop practice codes.

---
+++ src/ex_10_For_Loop/Lab113_For_Loop_Continue_Even_Numbers.java
@@ -0,0 +1,12 @@
+package ex_10_For_Loop;
+
+public class Lab113_For_Loop_Continue_Even_Numbers {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {// o to 50. Times-51
+            if (i % 2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/ex_10_For_Loop/Lab_112_For_Loop_Continue.java
@@ -0,0 +1,17 @@
+package ex_10_For_Loop;
+
+import java.util.Scanner;
+
+public class Lab_112_For_Loop_Continue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a no.");
+        int num = scanner.nextInt();
+        for (int i =0; i < num; i++) {
+            if (i == 7){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
